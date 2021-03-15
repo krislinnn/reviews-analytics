96828ba2ed9239f784c8bb7929e605764795b4c0
krislinnn <ohmygoood101@gmail.com>
2021-03-15 23:07:20 +0800

    add word counrt finction

--- read.py
+++ read.py
@@ -7,13 +7,57 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完了，總共有', len(data), '筆資料')
-sum_len = 0
-for d in data:
-	sum_len += len(d)
-print('留言的平均長度為', sum_len/len(data))
 
-new = []
+print(data[0])
+
+wc = {} #word_count
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆長度小於100')
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進字典
+
+	for word in wc:
+		if wc[word] > 1000000:
+			print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	input('請問你想查什麼字： ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為：', wc[word])
+	else:
+		print('這個字沒有出現過哦')
+
+print('感謝使用本查詢功能')
+
+# sum_len = 0
+# for d in data:
+# 	sum_len += len(d)
+# print('留言的平均長度為', sum_len/len(data))
+
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('一共有', len(new), '筆長度小於100')
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('一共有', len(good), '筆留言提到good')
+
+# good = [d for d in data if 'good' in d] #快寫法
+
+
+# bad = [d for d in data]
+# print(bad)
+
+
+
